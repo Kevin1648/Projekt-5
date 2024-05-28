30599c3952012b2329a3d21a6224521528d54998
Kevin1648 <153863004+Kevin1648@users.noreply.github.com>
2024-05-28 12:09:31 +0200

    Delete menu.js

--- css/menu.js
+++
@@ -1,9 +0,0 @@
-// menu.js
-document.addEventListener('DOMContentLoaded', function() {
-    const burgerMenu = document.getElementById('burger-menu');
-    const navigation = document.getElementById('navigation');
-
-    burgerMenu.addEventListener('click', function() {
-        navigation.classList.toggle('open');
-    });
-});
