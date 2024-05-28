e3a9013b85c57d0788e361ae10d83aefc3d5ba6c
Kevin1648 <153863004+Kevin1648@users.noreply.github.com>
2024-05-28 12:09:24 +0200

    Create menu.js

---
+++ javascript/menu.js
@@ -0,0 +1,31 @@
+{
+    const burgerMenu = document.getElementById('burger-menu');
+    
+
+    const navigation = document.getElementById('navigation');
+    
+
+    const navItems = Array.from(document.querySelectorAll('.nav-item'));
+   
+    burgerMenu.addEventListener('click', function() {
+        
+
+        if (navigation.classList.contains('open')) {
+           
+
+            navItems.forEach(function(item) {
+                item.style.display = 'none';
+              
+            });
+        } else {
+           
+
+            navItems.forEach(function(item) {
+                item.style.display = 'block';
+             
+            });
+        }
+     
+        navigation.classList.toggle('open');
+    });
+};
